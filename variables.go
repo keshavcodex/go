05513193c206ddb3183dc13c362c2be185836de8
keshavcodex <keshavcodex@gmail.com>
2023-05-05 20:08:00 +0530

    basic snippets

---
+++ variables.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var initial = "initial";
+	fmt.Println(initial)
+
+	var a, b int = 1, 2
+	fmt.Println(a,b)
+
+	c := true
+	fmt.Println(c)
+
+	f := "apple"
+	fmt.Println(f)
+
+}
